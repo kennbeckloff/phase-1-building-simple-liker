325628530de50b334ce77fe5593a4192a64c811c
kennbeckloff <kennbeckloff688@gmail.com>
2022-06-16 17:06:56 +0300

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modalDiv = document.querySelector("#modal");
+modalDiv.classList.add("hidden");
 
+const clickHeart = (event) =>{
+  const heart = event.target
+  heart.innerText = EMPTY_HEART
+  mimicServerCall()
+    .then( res => {
+      if(heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.classList.add("activated-heart")
+      }
+      else{
+        heart.innerText = EMPTY_HEART
+        heart.classList.remove("activated-heart")
+      }
+      
+      
+    })
+    .catch(err => {
+      modalDiv.classList.remove("hidden");
+      modalDiv.querySelector("#modal-message").textContent = err;
+      setTimeout( () => {
+        modalDiv.classList.add("hidden")
+      }, 3000)
+    }) 
+  
+}
+
+
+const hearts = document.querySelectorAll(".like-glyph");
+hearts.forEach( heart => {
+  heart.addEventListener("click", clickHeart)
+})
 
 
 
